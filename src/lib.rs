42740df0a60cf773af5368d71eab1cefda7f3abd
Sam Harrison <samharrison@cs.toronto.edu>
2025-09-16 09:53:51 -0400

    feat: very basic test suite

--- src/lib.rs
+++ src/lib.rs
@@ -4,22 +4,42 @@ mod tensor_core;
 mod tensor_pg;
 
 use pgrx::prelude::*;
+use std::error::Error;
 
 ::pgrx::pg_module_magic!(name, version);
-
-#[pg_extern]
-fn hello_pgtensor() -> &'static str {
-    "Hello, pgtensor"
-}
 
 #[cfg(any(test, feature = "pg_test"))]
 #[pg_schema]
 mod tests {
+    use std::error::Error;
     use pgrx::prelude::*;
 
+    use crate::tensor_core::{self, to_literal};
+
     #[pg_test]
-    fn test_hello_pgtensor() {
-        assert_eq!("Hello, pgtensor", crate::hello_pgtensor());
+    fn test_create_table() -> Result<(), Box<dyn Error>> {
+        Spi::run(
+            "CREATE TABLE t (x tensor(2,3));
+
+            INSERT INTO t VALUES ('[[0,1,2],[2,3,4]]');
+
+            SELECT x FROM t;
+            "
+        );
+        Ok(())
+    }
+
+    #[pg_test]
+    fn test_select()-> Result<(), Box<dyn Error>>  {
+        let t = Spi::get_one::<tensor_core::Tensor>("SELECT '[1]'::tensor")?.unwrap();
+        assert_eq!(t.ndims, 1);
+        assert_eq!(t.dims, Vec::from([1]));
+        assert_eq!(t.dtype, tensor_core::DataType::Float64);
+        assert_eq!(t.nelems, 1);
+        assert_eq!(t.strides, Vec::from([tensor_core::DataType::Float64.size_of() as u32]));
+        assert_eq!(t.buffer, (1.0 as f64).to_le_bytes());
+        assert_eq!(to_literal(&t), "[1.0]".to_owned());
+        Ok(())
     }
 }
 
